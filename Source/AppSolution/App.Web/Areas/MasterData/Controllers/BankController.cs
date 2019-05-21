466083c40abad3aedfeae61e8a65a78c44f776f5
sarah <saraaahhadid95@gmail.com>
2019-05-21 15:52:40 +0700

    Update master data (bank)
    masih eror saat memanggil foldernya.

---
+++ Source/AppSolution/App.Web/Areas/MasterData/Controllers/BankController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using App.Entities.DataAccessLayer;
+using App.Entities.Models;
+using App.Web.Models;
+using App.Web.Areas.MasterData.Models;
+using App.Entities;
+
+namespace App.Web.Areas.MasterData.Controllers
+{
+    public class BankController : Controller
+    {
+        // GET: MasterData/Bank
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/AppSolution/App.Web/Areas/MasterData/Models/BankSearchForm.cs
@@ -0,0 +1,19 @@
+﻿using App.Entities.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Web.Areas.MasterData.Models
+{
+    public class BankSearchForm : Controller
+    {
+        // GET: MasterData/BankSearchForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
