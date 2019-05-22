da04eaa116a87263e2b736afc7f86aa23c2ff478
admin <siti.marfuah@dac-solution.com>
2019-05-22 12:06:10 +0700

    Add Bank Model

---
+++ Source/AppSolution/App.Entities/Models/Bank.cs
@@ -0,0 +1,22 @@
+﻿using App.Entities.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace App.Entities.Models
+{
+    public class Bank
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Bank Id")]
+        public int BankId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Bank Name")]
+        public string Bankname { get; set; }
+    }    
+}
