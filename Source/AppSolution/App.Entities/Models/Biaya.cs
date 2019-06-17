5723fbdc51c0d480230524f701b02b5e1f9867ec
sarah <saraaahhadid95@gmail.com>
2019-06-17 13:06:05 +0700

    masterdata biaya
    error create.

--- Source/AppSolution/App.Entities/Models/Biaya.cs
+++ Source/AppSolution/App.Entities/Models/Biaya.cs
@@ -2,26 +2,37 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
-namespace App.Entities.Models
+namespace App.Web.Areas.MasterData.Models
 {
-    public class Biaya
+    public class CreateBiaya
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Display(Name = "Biaya Id")]
-        public int BiayaId { get; set; }
 
+        [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Kategori")]
         public string KatBiaya { get; set; }
 
-        [Display(Name = "Tingkat Id")]
-        public int? TingkatId { get; set; }
+        public IEnumerable<SelectListItem> Biayas { get; set; }
+
+        [Display(Name = "Jenis")]
+        public string JenisBiaya { get; set; }
+
+        [Display(Name = "Nominal")]
+        public string NomBiaya { get; set; }
 
         [Display(Name = "Tingkat")]
-        public virtual Tingkat Tingkats { get; set; }
+        public int? TingkatId { get; set; }
+
+        public SelectList Tingkats()
+        {
+            DatabaseContext db = new DatabaseContext();
+            var Tingkats = db.Tingkats;
+
+            return new SelectList(Tingkats.ToList(), "TingkatId", "Namatingkat", "0");
+        }
     }
 }
--- Source/AppSolution/App.Web/Areas/MasterData/Controllers/BiayaController.cs
+++ Source/AppSolution/App.Web/Areas/MasterData/Controllers/BiayaController.cs
@@ -27,7 +27,7 @@ namespace App.Web.Areas.MasterData.Controllers
             return View(model);
         }
 
-        //GET : MasterData/Tingkat/Create
+        //GET : MasterData/Biaya/Create
         public ActionResult Create()
         {
             CreateBiaya model = new CreateBiaya();
@@ -50,98 +50,99 @@ namespace App.Web.Areas.MasterData.Controllers
             ViewBag.JenisItem = ObjJenis;
             ViewBag.ListItem = ObjItem;
             return View(model);
-    }
+        }
 
-       
-        ////POST : MasterData/Tingkat/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(CreateBiaya model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Biaya newmodel = new Biaya();
-        //        newmodel.KatBiaya = model.KatBiaya;
-        //        newmodel.TingkatId = model.TingkatId;
+        //POST : Masterdata/Biaya/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateBiaya model)
+        {
+            if (ModelState.IsValid)
+            {
+                Biaya newmodel = new Biaya();
+                newmodel.KatBiaya = model.KatBiaya;
+                newmodel.JenisBiaya = model.JenisBiaya;
+                newmodel.TingkatId = model.TingkatId;
+                newmodel.NomBiaya = model.NomBiaya;
 
-        //        db.Biayas.Add(newmodel);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+                //db.Biayas.Add(newmodel);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
         //[HttpGet]
-        //public ActionResult AjaxBiaya(JQueryDataTableParamModel param)
-        //{
-        //    var QS = Request.QueryString;
-        //    String KatBiaya = QS["KatBiaya"];
-        //    //Boolean IsActive = (QS["IsActive"] == "false" ? false : true);
+        public ActionResult AjaxBiaya(JQueryDataTableParamModel param)
+        {
+            var QS = Request.QueryString;
+            String KatBiaya = QS["KatBiaya"];
+            //Boolean IsActive = (QS["IsActive"] == "false" ? false : true);
 
-        //    List<string[]> listResult = new List<string[]>();
-        //    String errorMessage = "";
+            List<string[]> listResult = new List<string[]>();
+            String errorMessage = "";
 
-        //    try
-        //    {
-        //        IEnumerable<Biaya> Query = db.Biayas;
-        //        if (KatBiaya != "")
-        //        {
-        //            Query = Query.Where(x => x.KatBiaya.Contains(KatBiaya));
-        //        }
-        //        //if (Jenjang != null)
-        //        //{
-        //        //    Query = Query.Where(x => x.Jenjang.Contains(Jenjang));
-        //        //}
+            try
+            {
+                IEnumerable<Biaya> Query = db.Biayas;
+                if (KatBiaya != "")
+                {
+                    Query = Query.Where(x => x.KatBiaya.Contains(KatBiaya));
+                }
+                //if (Jenjang != null)
+                //{
+                //    Query = Query.Where(x => x.Jenjang.Contains(Jenjang));
+                //}
 
 
-        //        int TotalRecord = Query.Count();
+                int TotalRecord = Query.Count();
 
-        //        var OrderedQuery = Query.OrderBy(x => x.BiayaId);
+                var OrderedQuery = Query.OrderBy(x => x.BiayaId);
 
-        //        int pageSize = param.iDisplayLength;
-        //        int pageNumber = param.iDisplayStart == 0 ? 1 : (param.iDisplayStart / param.iDisplayLength) + 1;
-        //        var PagedQuery = OrderedQuery.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
+                int pageSize = param.iDisplayLength;
+                int pageNumber = param.iDisplayStart == 0 ? 1 : (param.iDisplayStart / param.iDisplayLength) + 1;
+                var PagedQuery = OrderedQuery.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
 
-        //        int i = 0;
-        //        foreach (var data in PagedQuery)
-        //        {
-        //            i++;
-        //            listResult.Add(new string[]
-        //            {
-        //                i.ToString(),
-        //                data.KatBiaya,
-        //                data.JenisBiaya,
-        //                data.Tingkats.TingkatId,
-        //                data.NomBiaya,
-        //                //data.Jenjang,
-        //                //(data.IsActive == true ? "<input type=\"checkbox\" disabled checked>" : "<input type=\"checkbox\" disabled>"),
-        //                data.BiayaId.ToString()
-        //            });
-        //        }
-        //        return Json(new
-        //        {
-        //            sEcho = param.sEcho,
-        //            iTotalRecords = TotalRecord,
-        //            iTotalDisplayRecords = TotalRecord,
-        //            aaData = listResult
-        //        },
-        //        JsonRequestBehavior.AllowGet);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        errorMessage = ex.Message;
-        //    }
+                int i = 0;
+                foreach (var data in PagedQuery)
+                {
+                    i++;
+                    listResult.Add(new string[]
+                    {
+                        i.ToString(),
+                        data.KatBiaya,
+                        //data.JenisBiaya,
+                        //data.Tingkats.TingkatId,
+                        //data.NomBiaya,
+                        //data.Jenjang,
+                        //(data.IsActive == true ? "<input type=\"checkbox\" disabled checked>" : "<input type=\"checkbox\" disabled>"),
+                        data.BiayaId.ToString()
+                    });
+                }
+                return Json(new
+                {
+                    sEcho = param.sEcho,
+                    iTotalRecords = TotalRecord,
+                    iTotalDisplayRecords = TotalRecord,
+                    aaData = listResult
+                },
+                JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
 
-        //    return Json(new
-        //    {
-        //        sEcho = param.sEcho,
-        //        iTotalRecords = 0,
-        //        iTotalDisplayRecords = 0,
-        //        aaData = listResult,
-        //        error = errorMessage
-        //    },
-        //    JsonRequestBehavior.AllowGet);
-        //}
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = 0,
+                iTotalDisplayRecords = 0,
+                aaData = listResult,
+                error = errorMessage
+            },
+            JsonRequestBehavior.AllowGet);
+        }
     }
 }
