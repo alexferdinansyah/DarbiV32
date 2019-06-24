5d19853031cd65da53f7b2a7e393e43ffe6c89ca
sarah <saraaahhadid95@gmail.com>
2019-06-24 12:25:38 +0700

    registrasi siswa

---
+++ Source/AppSolution/App.Web/Areas/Register/Controllers/RegSiswaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Web.Areas.Register.Controllers
+{
+    public class RegSiswaController : Controller
+    {
+        // GET: Register/RegSiswa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/AppSolution/App.Web/Areas/Register/Models/RegSiswaSearchFormVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Web.Areas.Register.Models
+{
+    public class RegSiswaSearchFormVM : Controller
+    {
+        // GET: Register/RegSiswaSearchFormVM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
